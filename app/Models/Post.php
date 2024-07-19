c1ee898a147a7b680dc04d49ddf93ee5778919f2
Olumide Olugbemiro <olugbemiro.olumide@gmail.com>
2024-07-19 11:50:36 +0100

    added post model and scope for unpublished posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Scopes\PublishedScope;
 
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'author',
+        'publish_at'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'publish_at' => 'datetime',
+    ];
+
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new PublishedScope);
+    }
 }
