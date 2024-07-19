c89ebc49aadaadbb271b7e264120ff4eaebac5db
Olumide Olugbemiro <olugbemiro.olumide@gmail.com>
2024-07-19 12:13:55 +0100

    added post scheduling feature

---
+++ app/Console/Commands/PublishScheduledPosts.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Post;
+use Illuminate\Support\Facades\Log;
+
+class PublishScheduledPosts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:publish-scheduled-posts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish scheduled posts';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $posts = Post::withoutGlobalScopes()->where('publish_at', '<=', now())->get();
+
+        foreach ($posts as $post) {
+            $post->update(['publish_at' => null]);
+        }
+
+        Log::info('Scheduled posts published successfully.');
+    }
+}
