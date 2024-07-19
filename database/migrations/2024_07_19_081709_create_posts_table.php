efd37ec13aa0d0febb9de21d4393b7a40e749116
Olumide Olugbemiro <olugbemiro.olumide@gmail.com>
2024-07-19 09:18:45 +0100

    added migration for post table

--- database/migrations/2024_07_19_081709_create_posts_table.php
+++ database/migrations/2024_07_19_081709_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('author');
+            $table->timestamp('publish_at')->nullable();
             $table->timestamps();
         });
     }
